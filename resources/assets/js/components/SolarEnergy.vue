7cc1f14d8f24747e5fc7d49f3e108d786fa6d38e
John <jbraunnl@gmail.com>
2018-04-04 20:11:33 +0200

    Improved styles for mobile interfaces

--- resources/assets/js/components/SolarEnergy.vue
+++ resources/assets/js/components/SolarEnergy.vue
@@ -1,23 +1,16 @@
 <template>
-  <div class="energy-container">
-    <div class="card circle-container">
-        <div class="circle-content">
-          <h3>Today</h3>
-          <div class="power-text"><span class="power-value">{{ this.energy.eday ? this.energy.eday.split('kWh')[0] : '...'}}</span></div> kWh
-        </div>
+  <div class="grid-container">
+    <div class="grid-item">
+      <strong>Today</strong><br>
+      <span class="number">{{ this.energy.eday ? this.energy.eday.split('kWh')[0] : '...' }}</span> <br> kWh
     </div>
-    <div class="card circle-container">
-      <div class="circle-content">
-        <h3 class="desktop">Generating</h3>
-        <h3 class="mobile">Now</h3>
-        <span class="power-value">{{ nowGenerating }}</span> W
-      </div>
+        <div class="grid-item">
+      <strong>Now</strong><br>
+      <span class="number">{{ nowGenerating }}</span> <br> W
     </div>
-    <div class="card circle-container">
-      <div class="circle-content">
-        <h3>Total</h3>
-         <span class="power-value">{{ this.energy.etotal ? this.energy.etotal.split('kWh')[0] : '...' }}</span> kWh
-      </div>
+        <div class="grid-item">
+      <strong>Total</strong><br>
+        <span class="number">{{ this.energy.etotal ? this.energy.etotal.split('kWh')[0] : '...' }}</span> <br> kWh
     </div>
   </div>
 </template>
