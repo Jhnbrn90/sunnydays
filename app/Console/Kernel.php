fa360349839453b6635634057d82eac294a9e428
John Braun <jbraunnl@gmail.com>
2019-01-20 13:08:19 +0100

    Set the cron job back to every fifteen minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends ConsoleKernel
                     ]);
                 }
             }
-        })->everyMinute();
+        })->everyFifteenMinutes();
 
         $schedule->call(function () {
             $url = \Config::get('app.url') . '/api/daily';
