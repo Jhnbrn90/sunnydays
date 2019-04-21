b07dcf424fba3f3cf7f442c70a1e456ea656a0dd
John Braun <jbraunnl@gmail.com>
2019-04-21 14:04:41 +0200

    Increase CSS version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/app.css?v=2">
     <link rel="stylesheet" href="css/weather-icons.css">
 
     <style>
