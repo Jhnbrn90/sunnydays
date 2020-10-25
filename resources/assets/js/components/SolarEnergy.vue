1751361e2b6980fb1783f271103732cc273f1b8e
John Braun <john@brn.email>
2020-10-25 21:24:05 +0100

    WIP: refactor SolarEnergyComponent

--- resources/assets/js/components/SolarEnergy.vue
+++ resources/assets/js/components/SolarEnergy.vue
@@ -1,115 +1,165 @@
 <template>
   <div>
-    <center><strong>
+    <div class="w-full text-center mb-2">
       <span class="JL">&mdash; J&amp;L</span>
       <span class="MB">&mdash; M&amp;B</span>
       <span class="BE">&mdash; B&amp;E</span>
       <span class="RB">&mdash; Ron</span>
-      </strong>
-    </center>
-    <br>
-  <div class="grid-container">
-      <div class="grid-item">
-      <strong>Today</strong><br>
-      kWh
-      <div v-for="user in Object.keys(goodweIds)" v-if="!loading">
-        <span :class='classes(user)'>
-          {{ energy[user] ? energy[user].eday : '...' }}
-        </span> <br>
-      </div>
-      <div v-if="loading">
-        <br>
-        <center>
-        <breeding-rhombus-spinner
-          :animation-duration="2000"
-          :size="30"
-          color="#029ee3"
-        />
-        </center>
-        <br>
-        Loading...
-      </div>
-
     </div>
 
-    <div class="grid-item">
-      <strong>Now</strong><br>
-      W
-      <div v-for="user in Object.keys(goodweIds)"  v-if="!loading">
-      <span :class="classes(user)">
-        {{ nowGenerating(user) }}
-      </span> <br>
+    <div class="w-full flex items-center justify-center">
+      <div class="w-1/3 flex justify-center">
+        <div class="rounded-xl border border-gray-600 shadow px-3 py-2 sm:p-4 text-center">
+          <span class="font-semibold sm:font-medium d-block text-lg sm:text-2xl tracking-wider">Today</span>
+          <span class="font-thin">kWh</span>
+
+          <div v-for="user in Object.keys(goodweIds)" v-if="!loading">
+            <span :class='classes(user)'>
+              {{ energy[user] ? energy[user].eday : '...' }}
+            </span> <br>
+          </div>
+
+          <div v-if="loading">
+            <br>
+            <center>
+              <breeding-rhombus-spinner
+                  :animation-duration="2000"
+                  :size="30"
+                  color="#029ee3"
+              />
+            </center>
+            <br>
+            Loading...
+          </div>
+        </div>
       </div>
 
-      <div v-if="loading">
-        <br>
-        <center>
-        <breeding-rhombus-spinner
-          :animation-duration="2000"
-          :size="30"
-          color="#ffa578"
-        />
-        </center>
-        <br>
-        Loading...
+      <div class="w-1/3 flex justify-center">
+        <div>
+          2
+        </div>
       </div>
 
+      <div class="w-1/3 flex justify-center">
+        <div>
+          3
+        </div>
+      </div>
     </div>
-
-    <div class="grid-item">
-      <strong>Total</strong><br>
-      kWh
-      <div v-for="user in Object.keys(goodweIds)"  v-if="!loading">
-        <span :class="classes(user)">
-          {{ energy[user] ? energy[user].etotal : '...' }}
-        </span> <br>
-      </div>
-      
-      <div v-if="loading">
-        <br>
-        <center>
-        <breeding-rhombus-spinner
-          :animation-duration="2000"
-          :size="30"
-          color="#009933"
-        />
-        </center>
-        <br>
-        Loading...
-      </div>
-
-    </div>
-
-        <div class="grid-item">
-      <strong>Average</strong><br>
-      kWh /day
-      <div v-for="user in Object.keys(goodweIds)"  v-if="!loading">
-        <span :class="classes(user)">
-          {{ average[user] ? average[user] : '' }}
-        </span> <br>
-      </div>
-      
-      <div v-if="loading">
-        <br>
-        <center>
-        <breeding-rhombus-spinner
-          :animation-duration="2000"
-          :size="30"
-          color="#992300"
-        />
-        </center>
-        <br>
-        Loading...
-      </div>
-
-    </div>
-
   </div>
-  </div>
+
+
+  <!--  <div>-->
+  <!--    <center><strong>-->
+  <!--      <span class="JL">&mdash; J&amp;L</span>-->
+  <!--      <span class="MB">&mdash; M&amp;B</span>-->
+  <!--      <span class="BE">&mdash; B&amp;E</span>-->
+  <!--      <span class="RB">&mdash; Ron</span>-->
+  <!--      </strong>-->
+  <!--    </center>-->
+  <!--    <br>-->
+  <!--  <div class="grid-container">-->
+  <!--      <div class="grid-item">-->
+  <!--      <strong>Today</strong><br>-->
+  <!--      kWh-->
+  <!--      <div v-for="user in Object.keys(goodweIds)" v-if="!loading">-->
+  <!--        <span :class='classes(user)'>-->
+  <!--          {{ energy[user] ? energy[user].eday : '...' }}-->
+  <!--        </span> <br>-->
+  <!--      </div>-->
+  <!--      <div v-if="loading">-->
+  <!--        <br>-->
+  <!--        <center>-->
+  <!--        <breeding-rhombus-spinner-->
+  <!--          :animation-duration="2000"-->
+  <!--          :size="30"-->
+  <!--          color="#029ee3"-->
+  <!--        />-->
+  <!--        </center>-->
+  <!--        <br>-->
+  <!--        Loading...-->
+  <!--      </div>-->
+
+  <!--    </div>-->
+
+  <!--    <div class="grid-item">-->
+  <!--      <strong>Now</strong><br>-->
+  <!--      W-->
+  <!--      <div v-for="user in Object.keys(goodweIds)"  v-if="!loading">-->
+  <!--      <span :class="classes(user)">-->
+  <!--        {{ nowGenerating(user) }}-->
+  <!--      </span> <br>-->
+  <!--      </div>-->
+
+  <!--      <div v-if="loading">-->
+  <!--        <br>-->
+  <!--        <center>-->
+  <!--        <breeding-rhombus-spinner-->
+  <!--          :animation-duration="2000"-->
+  <!--          :size="30"-->
+  <!--          color="#ffa578"-->
+  <!--        />-->
+  <!--        </center>-->
+  <!--        <br>-->
+  <!--        Loading...-->
+  <!--      </div>-->
+
+  <!--    </div>-->
+
+  <!--    <div class="grid-item">-->
+  <!--      <strong>Total</strong><br>-->
+  <!--      kWh-->
+  <!--      <div v-for="user in Object.keys(goodweIds)"  v-if="!loading">-->
+  <!--        <span :class="classes(user)">-->
+  <!--          {{ energy[user] ? energy[user].etotal : '...' }}-->
+  <!--        </span> <br>-->
+  <!--      </div>-->
+  <!--      -->
+  <!--      <div v-if="loading">-->
+  <!--        <br>-->
+  <!--        <center>-->
+  <!--        <breeding-rhombus-spinner-->
+  <!--          :animation-duration="2000"-->
+  <!--          :size="30"-->
+  <!--          color="#009933"-->
+  <!--        />-->
+  <!--        </center>-->
+  <!--        <br>-->
+  <!--        Loading...-->
+  <!--      </div>-->
+
+  <!--    </div>-->
+
+  <!--        <div class="grid-item">-->
+  <!--      <strong>Average</strong><br>-->
+  <!--      kWh /day-->
+  <!--      <div v-for="user in Object.keys(goodweIds)"  v-if="!loading">-->
+  <!--        <span :class="classes(user)">-->
+  <!--          {{ average[user] ? average[user] : '' }}-->
+  <!--        </span> <br>-->
+  <!--      </div>-->
+  <!--      -->
+  <!--      <div v-if="loading">-->
+  <!--        <br>-->
+  <!--        <center>-->
+  <!--        <breeding-rhombus-spinner-->
+  <!--          :animation-duration="2000"-->
+  <!--          :size="30"-->
+  <!--          color="#992300"-->
+  <!--        />-->
+  <!--        </center>-->
+  <!--        <br>-->
+  <!--        Loading...-->
+  <!--      </div>-->
+
+  <!--    </div>-->
+
+  <!--  </div>-->
+  <!--  </div>-->
 </template>
 
 <script>
-import { BreedingRhombusSpinner } from 'epic-spinners'
+import {BreedingRhombusSpinner} from 'epic-spinners'
 
 export default {
   props: ['goodweIds', 'api'],
@@ -145,14 +195,14 @@ export default {
       this.getAverage();
 
       axios.get('/api/powerstations')
-        .then(response => {
-          this.energy = response.data;
-          this.loading = false;
-      });
+          .then(response => {
+            this.energy = response.data;
+            this.loading = false;
+          });
     },
 
     getAverage() {
-      axios.get('/api/average-yield').then(({ data }) => {
+      axios.get('/api/average-yield').then(({data}) => {
         this.average = data;
       });
     },
@@ -169,3 +219,20 @@ export default {
 };
 </script>
 
+<style>
+.JL {
+  color: rgb(255, 165, 120);
+}
+
+.MB {
+  color: rgb(2, 158, 227);
+}
+
+.BE {
+  color: rgb(0, 153, 51)
+}
+
+.RB {
+  color: rgb(95, 66, 244);
+}
+</style>
