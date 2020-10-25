51f5f4c398ef4d856a50780c6ffd56783a8d9dd0
John Braun <john@brn.email>
2020-10-25 21:21:52 +0100

    Extract layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="Sunny Days">
+    <meta name="application-name" content="Sunny Days">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Sunny Days🌤 </title>
+
+    <link rel="stylesheet" href="{{ mix("css/app.css") }}">
+    <link rel="stylesheet" href="css/weather-icons.css">
+</head>
+
+<body class="bg-white">
+    <div id="app">
+        {{ $slot }}
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
