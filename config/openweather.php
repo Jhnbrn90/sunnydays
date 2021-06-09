3e1977392775ddba72bc370b718543a5b8d2af32
John Braun <john@brn.email>
2021-06-09 15:26:07 +0200

    Use open weather image from https source

--- config/openweather.php
+++ config/openweather.php
@@ -4,6 +4,6 @@ return [
     'location' => 'Hoofddorp,nl',
     'key' => env('OPENWEATHER_KEY', ''),
     'url' => 'https://api.openweathermap.org/data/2.5/weather',
-    'icon_base_url' => 'http://openweathermap.org/img/wn',
+    'icon_base_url' => 'https://openweathermap.org/img/wn',
     'icon_extension' => '@2x.png',
 ];
